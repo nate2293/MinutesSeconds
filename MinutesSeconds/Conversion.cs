5000e06d347743ff5566f049e045e8161e334aa6
nate2293 <Mc_Garvey-N@ulster.ac.uk>
2024-01-09 20:26:13 +0000

    Add project files.

---
+++ MinutesSeconds/Conversion.cs
@@ -0,0 +1,28 @@
+﻿namespace MinutesSeconds
+{
+    internal class Conversion
+    {
+        public static int Convert(int minutes)      // Expects an int and outputs an int.
+        {
+            return minutes * 60;        
+        }
+
+        public static void Main()
+        {
+            int resultOne = Convert(6);
+            Console.WriteLine(resultOne);
+            Console.WriteLine();
+
+            int resultTwo = Convert(15);
+            Console.WriteLine(resultTwo);
+            Console.WriteLine();
+
+            int resultThree = Convert(5);
+            Console.WriteLine(resultThree);
+            Console.WriteLine();
+
+        }
+    }
+}
+
+// Create a method that converts minutes into seconds and display output.
